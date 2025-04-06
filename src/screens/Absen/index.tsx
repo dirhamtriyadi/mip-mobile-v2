0e0aa7f86a6aece4b5233c267c8b63e8e0dbf6dc
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-04-06 09:48:03 +0700

    style(absen): update import and add container style

--- src/screens/Absen/index.tsx
+++ src/screens/Absen/index.tsx
@@ -1,34 +1,43 @@
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { RootStackParamList } from "../../../App";
-import globalStyles from "@styles/styles";
-import styles from "./styles";
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RootStackParamList } from '../../../App';
+import styles from './styles';
 
 function AbsenScreen() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
-  
+
   const handleClick = (navigate: any) => {
     navigation.navigate(navigate);
-  }
+  };
 
   return (
-    <SafeAreaView style={globalStyles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.listButton}>
-        <TouchableOpacity style={styles.btn} onPress={() => handleClick('AbsenMasuk')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => handleClick('AbsenMasuk')}>
           <Text>Absen Masuk</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={() => handleClick('AbsenPulang')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => handleClick('AbsenPulang')}>
           <Text>Absen Pulang</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={() => handleClick('Sakit')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => handleClick('Sakit')}>
           <Text>Sakit</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={() => handleClick('Izin')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => handleClick('Izin')}>
           <Text>Izin</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={() => handleClick('Cuti')}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => handleClick('Cuti')}>
           <Text>Cuti</Text>
         </TouchableOpacity>
       </View>
