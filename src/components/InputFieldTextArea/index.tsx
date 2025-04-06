a56b22352bef207e32f5a75c8299aff8d43b6f51
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-04-06 17:30:52 +0700

    style(InputFieldTextArea): update import order and styles

--- src/components/InputFieldTextArea/index.tsx
+++ src/components/InputFieldTextArea/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import styles from './styles';
 
