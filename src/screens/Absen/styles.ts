5df3c586dec74b0a069e0fc42034a1aabf5b1622
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-04-06 09:54:24 +0700

    style(Absen): remove backgroundColor from container

--- src/screens/Absen/styles.ts
+++ src/screens/Absen/styles.ts
@@ -4,7 +4,6 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flex: 1,
-    backgroundColor: 'blue',
     width: '100%',
     height: '100%',
     justifyContent: 'center',
